80c17c00d2a8cfa079981876e865ac7862fa5f30
Fullooh <angstdemon1@aol.com>
2022-08-28 20:08:23 -0400

    Update to repo

--- Lab1/p3.py
+++ Lab1/p3.py
@@ -10,11 +10,17 @@ wn = turtle.Screen()
 thomasH = turtle.Turtle()
 
 for i in range(9):
+     thomasH.pencolor("purple")
      thomasH.forward(100)
+     thomasH.pencolor("blue")
      thomasH.left(105)
+     thomasH.pencolor("green")
      thomasH.forward(52)
+     thomasH.pencolor("red")
      thomasH.left(105)
+     thomasH.pencolor("blue")
      thomasH.forward(100)
+     thomasH.pencolor("cyan")
      thomasH.right(170)
 
 wn.exitonclick()
