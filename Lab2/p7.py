12aa797ac0c7a0527576fa01144287e5b335d050
Fullooh <angstdemon1@aol.com>
2022-08-31 16:25:05 -0400

    Update to repo

--- Lab2/p7.py
+++ Lab2/p7.py
@@ -11,4 +11,6 @@ print("name in LASTNAME, firstName format: " + x[1].upper() +", " + x[0])
 
 email_name = input("Enter user name of email: ")
 
-print("email: " + email_name + "@hunter.cuny.edu")
+i = email_name.split()
+
+print("email: " + i[0].lower() + "@hunter.cuny.edu")
---
+++ Lab2/p8.py
@@ -0,0 +1,9 @@
+#Name: Angst S Gregory
+#Email: angst.gregory17@myhunter.cuny.edu
+#Date: ------
+#This program prints: -----
+
+myString = input("Enter a Message: ")
+
+for i in myString:
+    print(i, ord(i), chr(ord(i) + 2))
