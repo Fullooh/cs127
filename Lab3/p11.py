5c05b018cc1ed73144d038e86ca765829e72d841
Fullooh <angstdemon1@aol.com>
2022-09-06 15:13:47 -0400

    Update to repo

---
+++ Lab3/p11.py
@@ -0,0 +1,14 @@
+#Name: Angst S Gregory
+#Email: angst.gregory17@myhunter.cuny.edu
+#Date: ------
+#This program prints: -----
+
+import turtle
+
+t = turtle.Turtle()
+t.penup()
+t.backward(100)
+t.left(90)
+t.backward(100)
+t.right(??) #you fill in a degree in ??
+t.pendown()
