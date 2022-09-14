87a5ba64a157c7cbb87ee882a806c98788572f8e
Angst Gregory <angstgregory@Ragnorak.local>
2022-09-14 15:18:36 -0400

    Update to repo

--- Lab3/p11.py
+++ Lab3/p11.py
@@ -7,22 +7,28 @@ import turtle
 
 turtle.colormode(255)
 a = turtle.Turtle()
-a.shape("turtle")
+
 a.penup()
+a.backward(100)
 a.left(90)
-a.pendown()
-for k in range(0,255,10):
- a.forward(10)
- a.pensize(k)
- a.color(0,k,k)
-a.penup()
-for k in range(255,0,-10):
- a.backward(10)
-a.pensize(0)
-a.color(0,0,0)
+a.backward(100)
 a.right(90)
 a.pendown()
+
+for k in range(0,255,10):
+    a.pensize(k)
+    a.color(0,k,k)
+    a.forward(10)
+
+
+a.penup()
+for k in range(255,0,-10):
+    a.backward(10)
+
+a.left(90)
+a.pendown()
+
 for k in range (0,255,10):
- a.forward(10)
- a.pensize(k)
- a.color(0,k,k)
+    a.pensize(k)
+    a.color(0,k,k)
+    a.forward(10)
