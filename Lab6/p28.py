5478ff54f24f12dc31359c65b428deb136c86f7d
Angst Gregory <angstgregory@Ragnorak-2.local>
2022-10-24 18:09:03 -0400

    Update to repo

--- Lab6/p28.py
+++ Lab6/p28.py
@@ -3,3 +3,15 @@
 #Date: ------
 #This program prints: ----
 
+#Import libraries.
+import pandas as pd
+
+#Read in the csv file.
+rain = pd.read_csv('AustraliaRain.csv')
+
+#Group the data by location
+groupedData = rain.groupby('Location')
+
+#Print the average rainfall
+print(groupedData['Rainfall'].mean())
+
