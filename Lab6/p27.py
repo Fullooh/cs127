8ab817de27631e97da10c15b22d0ddbc0066ebbc
Angst Gregory <angstgregory@Ragnorak-2.local>
2022-10-24 00:26:42 -0400

    Update to repo

---
+++ Lab6/p27.py
@@ -0,0 +1,32 @@
+#Name: Angst S Gregory
+#Email: angst.gregory17@myhunter.cuny.edu
+#Date: ------
+#This program prints: ----
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+file = input ("Enter a csv file: ")
+
+borough = input("Enter a borough (Bronx, Brooklyn, Queens, Manhattan, Staten Island): ")
+
+output = input ("Enter output name: ")
+
+i = pd.read_csv(file)
+
+print ("Min:", i[borough].min())
+
+print ("Max: ", i[borough].max ())
+
+print ("Mean: ", round(i[borough] .mean(),3))
+
+print ("Median: ", i[borough].median())
+
+print ("Median: ", round(i[borough].std(),3))
+
+i[' Fraction' ] = (i[borough]) / (i['case_count'])
+
+i.plot(x = 'date_of_interest', y = 'Fraction')
+
+fig = plt.gcf()
+fig.savefig(output)
