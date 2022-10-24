ee585db6edb89f34106d097c6c3c72397179fddb
Angst Gregory <angstgregory@Ragnorak-2.local>
2022-10-23 22:55:17 -0400

    Update to repo

--- Lab5/p25.py
+++ Lab5/p25.py
@@ -3,3 +3,27 @@
 #Date: ------
 #This program prints: ----
 
+binaryNum = ""
+
+binaryNum = input("Enter a string with 0 or 1 only: ")
+
+num = 0
+
+base = 2
+
+weight = 1
+
+length = len(binaryNum)
+
+ch = " "
+
+for i in binaryNum[::-1]:
+        ch = i
+        if ch == "1":
+            num += weight
+        elif ch!= "0":
+            print("Letter", ch, "is not allowed in binary string.")
+            exit()
+        weight *= base
+
+print(str(num))
