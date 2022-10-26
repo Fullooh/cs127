9a746a49cc6492cb6e4b56a6192674882cbdb4df
Angst Gregory <angstgregory@Ragnorak-2.local>
2022-10-26 19:54:41 -0400

    Update to repo

--- Lab6/p28.py
+++ Lab6/p28.py
@@ -4,14 +4,41 @@
 #This program prints: ----
 
 #Import libraries.
+import matplotlib as plt
 import pandas as pd
 
 #Read in the csv file.
-rain = pd.read_csv('AustraliaRain.csv')
+lead = pd.read_csv('children_lead.csv')
 
-#Group the data by location
-groupedData = rain.groupby('Location')
+choice = input("Enter a choice: a. group by borough b. group by year")
 
-#Print the average rainfall
-print(groupedData['Rainfall'].mean())
 
+
+if choice == 'a':
+    groupedData = lead.groupby('borough')
+    #Group the data by borough
+    i = (groupedData["affected_children"].mean())
+    print("average number of affected children by borough")
+    print(i)
+
+    x = input("Enter a borough: ")
+    aff = groupedData.get_group(x)
+
+    print( "average number of affected children of", x , "is" , aff["affected_children"].mean())
+    print( "min number of affected children of", x , "is" , aff["affected_children"].min())
+    print( "max number of affected children of", x , "is" , aff["affected_children"].max())
+
+elif choice == "b":
+
+    aff = lead.groupby("year")
+    print("average number of affected children my year")
+    print(aff['affected_children'].mean())
+    
+    year = int(input('Enter a year(2005 - 2016): '))
+    year_group = aff.get_group(year)
+    print("average number of affected children in", year, "is" , year_group["affected_children"].mean())
+    print("min number of affected children in", year, "is" , year_group["affected_children"].min())
+    print("max number of affected children in", year, "is" , year_group["affected_children"].max())
+
+else:
+    print("wrong choice")
---
+++ Lab6/p29.sh
@@ -0,0 +1,8 @@
+#Name: Angst S Gregory
+#Email: angst.gregory17@myhunter.cuny.edu
+#Date: ------
+#This program prints: ----
+
+#!/bin/bash
+echo "Hello!"
+echo "Greeting from $USER"
