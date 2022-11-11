9618861f9c7bbc8a3996d341176f04e354e4eec7
Angst Gregory <angstgregory@Ragnorak-2.local>
2022-11-11 04:20:46 -0500

    Update to repo

--- Lab8/p38.py
+++ Lab8/p38.py
@@ -3,3 +3,25 @@
 #Date: ------
 #This program prints: ----
 
+import turtle
+import math
+
+def drawPantegon(t, length, numEdges):
+   if numEdges > 0:
+        t.forward(length)
+        t.left(72)
+        drawPantegon(t,length,numEdges-1)
+
+def cornerPantegon(t, length):
+    if length >= 25:
+        drawPantegon(t, length, 5)
+        length = length//2
+        cornerPantegon(t,length)
+
+def nestedPantegon(t, length):
+    if length >= 25:
+        drawPantegon(t,length,5)
+        t.forward(length/2)
+        t.left(36)
+        pos = t.position()
+        nestedPantegon(t,length*math.sin(54/180*math.pi))
